38471c9f6f6e9fd0c2263f644f977167982081bf
jtzhao <jtzhao@freewheel.tv>
2019-04-19 18:23:05 +0800

    Leetcode: 34 Search For A Range

--- Leetcode/0034-SearchForARange/go/main.go
+++ Leetcode/0034-SearchForARange/go/main.go
@@ -4,55 +4,40 @@ import (
 	"fmt"
 )
 
-// Find the leftmost or one position past rightmost target in [low, high] of nums
-func extremeInsertionIndex(nums []int, target int, low int, high int, left bool) int {
+// Find the index of the leftmost or rightmost target inside nums
+// left == true means we're searching for the leftmost target; otherwise, search for the rightmost one.
+func extremeInsertionIndex(nums []int, target int, left bool) int {
+    low, high := 0, len(nums)
     for low < high {
         mid := (low + high) / 2
-        if nums[mid] > target || (left && target == nums[mid]) {
-            high = mid;
+        if target < nums[mid] || (target == nums[mid] && left == true) {
+            high = mid
         } else {
-            low = mid + 1;
+            low = mid + 1
         }
     }
-    return low
+    if left && low < len(nums) && nums[low] == target {
+        return low
+    } else if !left && low - 1 < len(nums) && nums[low-1] == target {
+        return low - 1
+    }
+    return -1
 }
 
 func searchRange(nums []int, target int) []int {
-    var ret []int = []int{-1, -1}
-    // First, use binary search to find one target
-    low, high := 0, len(nums) - 1
-    index := -1
-    for low <= high {
-        mid := (low + high) / 2
-        if nums[mid] == target {
-            index = mid
-            break
-        }
-        if nums[mid] < target {
-            low = mid + 1
-        } else {
-            high = mid - 1
-        }
+    res := []int{-1, -1}
+    leftmost := extremeInsertionIndex(nums, target, true)
+    if leftmost == -1 {
+        return res
     }
-    // No target found
-    if index == -1 {
-        return ret
-    }
-    // Find leftmost target
-    ret[0] = extremeInsertionIndex(nums, target, low, index, true)
-    if nums[ret[0]] != target {
-        ret[0] = -1
-    }
-    ret[1] = extremeInsertionIndex(nums, target, index, high, false) - 1
-    if nums[ret[1]] != target {
-        ret[1] = -1
-    }
-    return ret
+    res[0] = leftmost
+    rightmost := extremeInsertionIndex(nums, target, false)
+    res[1] = rightmost
+    return res
 }
 
 func main() {
-    var nums []int = []int{5, 7, 7, 8, 8, 10}
-    var target int = 8
-    ret := searchRange(nums, target)
-    fmt.Println(ret)
+    nums := []int{5,7,7,8,8,10}
+    res := searchRange(nums, 100)
+    fmt.Println(res)
 }
