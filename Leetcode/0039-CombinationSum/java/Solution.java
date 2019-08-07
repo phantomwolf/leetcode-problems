0902ee3fb0cac4482373655028b593e5c730c3cb
jtzhao <jtzhao@freewheel.tv>
2019-08-07 17:54:43 +0800

    Leetcode: 39 Combination Sum

---
+++ Leetcode/0039-CombinationSum/java/Solution.java
@@ -0,0 +1,52 @@
+import java.lang.*;
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> combinations = new ArrayList<List<Integer>>();
+        Stack<Integer> path = new Stack<Integer>();
+
+        path.push(0);
+        int sum = candidates[0];
+
+        while (!path.empty()) {
+            while (sum < target) {
+                path.push(path.peek());
+                sum += candidates[path.peek()];
+            }
+            if (sum == target) {
+                List<Integer> combination = new ArrayList<Integer>();
+                for (Integer i : path) {
+                    combination.add(candidates[i]);
+                }
+                combinations.add(combination);
+            }
+            // Move backward and choose a different path
+            while (!path.empty()) {
+                Integer i = path.pop();
+                sum -= candidates[i];
+                if (i < candidates.length-1) {
+                    i++;
+                    path.push(i);
+                    sum += candidates[i];
+                    break;
+                }
+            }
+        }
+        return combinations;
+    }
+
+    public static void main(String[] args) {
+        int[] candidates = new int[]{2, 3, 5};
+        int target = 8;
+        Solution s = new Solution();
+        List<List<Integer>> result = s.combinationSum(candidates, target);
+        for (List<Integer> l : result) {
+            System.out.printf("%d", l.get(0));
+            for (int i=1; i < l.size(); i++) {
+                System.out.printf("+ %d", l.get(i));
+            }
+            System.out.printf(" = %d\n", target);
+        }
+    }
+}
