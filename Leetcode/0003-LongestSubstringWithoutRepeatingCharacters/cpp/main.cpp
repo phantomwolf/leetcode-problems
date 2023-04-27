208b72cbd62a8bb9f860d2e924cd407589119b46
phantomwolf <phantomwolf47@gmail.com>
2023-04-27 16:26:28 -0700

    Leetcode 3 Longest Substring Without Repeating Characters

--- Leetcode/0003-LongestSubstringWithoutRepeatingCharacters/cpp/main.cpp
+++ Leetcode/0003-LongestSubstringWithoutRepeatingCharacters/cpp/main.cpp
@@ -1,41 +1,65 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <string>
 
-using namespace std;
-
-class Solution {
+class Solution1 {
 public:
-    int lengthOfLongestSubstring(string s) {
+    int lengthOfLongestSubstring(std::string s) {
+        std::vector<int> index(256);
+        std::fill_n(index.begin(), 256, -1);
+        int l = 0, r;
         int longest = 0;
-        int hash_table[128];
-        int i = 0, j;
-        int length;
-        fill_n(hash_table, 128, -1);
-        for (j = 0; j < s.size(); ++j) {
-            if (hash_table[s[j]] != -1) {
-                // calculate substring length
-                length = j - i;
-                if (longest < length)
+        for (r = 0; r < s.size(); r++) {
+            if (index[s[r]] != -1) {
+                // Add s[r] to sliding window will result in duplication
+                int length = r - l;
+                if (length > longest)
                     longest = length;
-                while (i < hash_table[s[j]]) {
-                    hash_table[s[i]] = -1;
-                    ++i;
+                while (l <= index[s[r]]) {
+                    index[s[l]] = -1;
+                    l++;
                 }
-                ++i;
             }
-            hash_table[s[j]] = j;
+            index[s[r]] = r;
         }
-        // edge case
-        length = j - i;
-        if (longest < length)
+        int length = r - l;
+        if (length > longest) {
             longest = length;
+        }
+        return longest;
+    }
+};
+
+class Solution2 {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        int count[256] = {0};
+        int repeated = 0;   // number of repated characters in current window
+        int l = 0;
+        int longest = 0;
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i]]++;
+            if (count[s[i]] == 2) {
+                repeated++;
+            }
+            if (repeated == 0 && i - l + 1 > longest) {
+                longest = i - l + 1;
+            }
+            while (l < i && repeated > 0) {
+                count[s[l]]--;
+                if (count[s[l]] == 1) {
+                    repeated--;
+                }
+                l++;
+            }
+        }
         return longest;
     }
 };
 
 int main()
 {
-    Solution s;
-    cout << s.lengthOfLongestSubstring("pwwke") << endl;
+    Solution1 s;
+    std::cout << s.lengthOfLongestSubstring("pwwke") << std::endl;
     return 0;
 }
